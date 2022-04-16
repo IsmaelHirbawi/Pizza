59f7706e8c3284fb656bc2d5c514a6af66e6d428
IsmaelHirbawi <looleeraish@gmail.com>
2022-04-16 03:02:33 +0300

    creat Dough class extends Pizza class

---
+++ PizzaApp/src/main/java/com/mycompany/pizzaapp/Dough.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pizzaapp;
+
+/**
+ *
+ * @author loole
+ */
+public class Dough extends Pizza{
+    private String sizeOfDough;
+    private String typeOfDough;
+    
+    public Dough(){
+        
+    }
+
+    public Dough(String sizeOfDough, String typeOfDough) {
+        this.sizeOfDough = sizeOfDough;
+        this.typeOfDough = typeOfDough;
+    }
+
+    public String getSizeOfDough() {
+        return sizeOfDough;
+    }
+
+    public void setSizeOfDough(String sizeOfDough) {
+        this.sizeOfDough = sizeOfDough;
+    }
+
+    public String getTypeOfDough() {
+        return typeOfDough;
+    }
+
+    public void setTypeOfDough(String typeOfDough) {
+        this.typeOfDough = typeOfDough;
+    }
+    
+    public String getInfo(){
+        return "The type of dough is : " + typeOfDough + " ,His size : " + sizeOfDough;
+    }
+}
